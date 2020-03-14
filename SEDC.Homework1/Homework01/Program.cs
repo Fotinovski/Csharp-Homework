cb73761626039259ba330c8465ff2419b7c686da
Darko <fotinovskidarko@gmail.com>
2020-03-14 14:44:31 +0100

    Homework

---
+++ SEDC.Homework1/Homework01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+namespace SEDC.CSharp.Homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Traffic Light....");
+            Thread.Sleep(1500);
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Red Light - STOP!" );
+            Thread.Sleep(1500);
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Yellow Light - Prepare.");
+            Thread.Sleep(1500);
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green Light - Go!");
+
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+}
