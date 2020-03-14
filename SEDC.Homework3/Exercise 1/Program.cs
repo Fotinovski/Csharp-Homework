da710e8ece6255a1259c0404b603ddf0e47c8eb1
Darko <fotinovskidarko@gmail.com>
2020-03-14 15:22:07 +0100

    homework 3

---
+++ SEDC.Homework3/Exercise 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter five numbers: ");
+
+            int numberOne;
+            bool isValid1 = int.TryParse(Console.ReadLine(), out numberOne);
+
+            int numberTwo;
+            bool isValid2 = int.TryParse(Console.ReadLine(), out numberTwo);
+
+            int numberThree;
+            bool isValid3 = int.TryParse(Console.ReadLine(), out numberThree);
+
+            int numberFour;
+            bool isValid4 = int.TryParse(Console.ReadLine(), out numberFour);
+            
+            int numberFive;
+            bool isValid5 = int.TryParse(Console.ReadLine(), out numberFive);
+
+            int[] arrayOfNumbers = new int[5] {numberOne,numberTwo,numberThree,numberFour,numberFive };
+
+            int sumOfNumbers = 0;
+
+            foreach (var item in arrayOfNumbers )
+            {
+                sumOfNumbers += item;
+            }
+
+            Console.WriteLine("The sum is: " + sumOfNumbers);
+
+            Console.ReadLine();
+        }
+    }
+}
